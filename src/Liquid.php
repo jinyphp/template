f299e542b783f989d8d6473a73c7a6595a0474dc
hojinlee <infohojin@gmail.com>
2018-06-18 04:15:15 +0900

    0.0.2

--- src/Liquid.php
+++ src/Liquid.php
@@ -2,7 +2,6 @@
 namespace Jiny\Template;
 
 use \Jiny\Core\Registry\Registry;
-//use Liquid\Template;
 
 class Liquid
 {
@@ -14,20 +13,19 @@ class Liquid
         //echo __CLASS__."객체를 생성합니다.<br>";
         $this->View = $view;
 
-        $this->$Liquid = new \Liquid\Template();
+
+        //$path = __DIR__;
+        //echo $path;
+        $this->Liquid = new \Liquid\Template();
     }
 
     public function Liquid($body, $data)
     {
         //echo __METHOD__."<br>";
-        // echo "Liquid 템플릿을 처리합니다.<br>";
-        $this->$Liquid->parse($body);
+        // 코드를 추출합니다.
+        $this->Liquid->parse($body);
 
-        //echo "<pre>";
-        //$data['name'] = "jiny";
-        //print_r($data);
-        //echo "</pre>";
-
-        return $this->$Liquid->render($data);
+        // 추출한 코드에 데이터를 처리합니다.
+        return $this->Liquid->render($data);
     }
 }
