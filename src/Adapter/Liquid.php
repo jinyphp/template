2a931b04282a5a9e1a30a5dc131bea0eeedeb8bd
hojinlee <infohojin@gmail.com>
2018-09-23 18:41:51 +0900

    refactory

--- src/Adapter/Liquid.php
+++ src/Adapter/Liquid.php
@@ -2,7 +2,10 @@
 namespace Jiny\Template\Adapter;
 
 use \Jiny\Core\Registry\Registry;
-
+/**
+ * 어뎁터 패턴
+ * liquid/liquid 페키지로 결합합니다.
+ */
 class Liquid
 {
     private $View;
@@ -10,36 +13,50 @@ class Liquid
 
     public function __construct($view)
     {
-        // \TimeLog::set(__CLASS__."가 생성이 되었습니다.");
-
         // 의존성 주입
         // View 클래스의 인스턴스르 저장합니다.
         $this->View = $view;
 
-        // 패티지 liquid/liquid
-        // 인스턴스를 생성합니다.
-        // $path = $this->View->Controller->Application->Config->data("ENV.path.view");
+        // 리소스 설정값을 변환하여, 인스턴스를 생성합니다.
+        $path = $this->path();        
+        $this->Liquid = $this->factory($path);
+
+    }
+
+    /**
+     * 레이아웃 결합을 위한 기본 루트 경로를 설정합니다.
+     */
+    private function path()
+    {
         $path = ROOTPATH.Registry::get("CONFIG")->data("ENV.path.pages");
-        $path = str_replace("/",DS,$path);
-        //echo $path ."<br>";
-        
+        $path = str_replace("/", DS, $path);
+        return $path;
+    }
+
+    /**
+     * 인스턴스를 생성합니다.
+     */
+    private function factory($path)
+    {
         \Liquid\Liquid::set('INCLUDE_ALLOW_EXT', true);
-        $this->Liquid = new \Liquid\Template($path);
-        // $this->Liquid->setCache(new Local());
+
+        // 인스턴스를 생성합니다.
+        return new \Liquid\Template($path);
     }
 
     /**
      * Liquid 랜더링을 처리합니다.
      */
     public function Liquid($body, $data)
-    {
-        // \TimeLog::set(__METHOD__);
-        
-        // 코드를 추출합니다.
+    {        
+        // ㅣLiquid 코드를 추출합니다.
         $this->Liquid->parse($body);
 
         // 추출한 코드에 데이터를 처리합니다.
         return $this->Liquid->render($data);
     }
 
+    /**
+     * 
+     */
 }
--- src/Template.php
+++ src/Template.php
@@ -14,45 +14,36 @@ class Template Extends Engine
     private $App;
     public $Processor;
 
-    public function __construct($view)
-    {
-        // \TimeLog::set(__CLASS__."가 생성이 되었습니다.");
+    private $_viewHtml;
 
+    public function __construct($view=null)
+    {
         // 의존성 주입
         // View 클래스의 인스턴스르 저장합니다.
-        $this->View = $view;
-        $this->App = $view->App;
+        $this->View = Registry::get("View"); //$view;
+        $this->App = Registry::get("App"); //$view->App;
 
         // 기본 템플릿 동작
         // 카페트
         //$Carpet = new \Jiny\Template\Carpet\Carpet();
         //$this->View->_body = $Carpet->parser($this->View->_body);
-
-
-/*
-        // 사용자 템플릿 엔진 적용
-        // 템플릿을 처리하여 뷰의 _body에 저장합니다.
-        $this->View->_body = $this->process($this->View->_body);
-        */
-
     }
 
-    // 템플릿을 처리합니다.
-    public function process($body)
+    /**
+     * 템플릿 엔진을 처리합니다.
+     */
+    public function process($html)
     {
-        // \TimeLog::set(__METHOD__);
         // 템플릿 엔진에 따라서 동작을 처리합니다.
         switch ($this->isEngine()) {
             // Liquid 템플릿 엔진처리
             case 'Liquid':
-                $this->Processor = new \Jiny\Template\Adapter\Liquid($this->View);
-                       
-                return $this->Processor->Liquid($body, $this->View->_data);
+                $this->Processor = new \Jiny\Template\Adapter\Liquid($this->View);    
+                $html->_body = $this->Processor->Liquid($html->_body, $html->_data);
+                break;
 
             default:
         }
-      
-        return $body;
     }
 
     /**
